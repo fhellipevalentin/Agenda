bd4e47540f365314e8699fc26d1d32fef4d1cdd0
Fhellipe Valentin <fhellipereis@outlook.com>
2022-05-10 17:14:16 -0300

    Upload de arquivo - Springboot

--- src/main/java/io/github/fhellipevalentin/agenda/model/entity/Contato.java
+++ src/main/java/io/github/fhellipevalentin/agenda/model/entity/Contato.java
@@ -5,6 +5,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Lob;
 
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -22,12 +23,14 @@ public class Contato {
 	@Column(length = 150, nullable = false)
 	private String nome;
 	
-	@Column
+	@Column(length = 150, nullable = false)
 	private String email;
 
-	@Column(nullable = false)
+	@Column
 	private Boolean favorito;
 	
-	
+	@Column
+	@Lob
+	private byte[] foto; 
 	
 }
